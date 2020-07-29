aa142e992218bac272138e98f4b1c6f9edcd9de4
Gamover <gamover@outlook.com>
2020-07-29 22:22:23 +0500

    fc

---
+++ src/Castor.Network/Protocol/CastorProtocolHeader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Castor.Network.Protocol
+{
+    public struct CastorProtocolHeader
+    {
+        public Guid AppIdTo;
+        public Guid AppIdFrom;
+        public int Size;
+    }
+}
